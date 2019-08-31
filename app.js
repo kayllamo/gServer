8524dfeeec37e4b1cc75912e4eeb379e2382c9ae
kaylajaograham <kaylajaograham@gmail.com>
2019-08-31 13:46:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan('common'));
+
+const appData = require('./playstore.js');
+
+// WORKSPACE BELOWWWW 
+
+app.get('/apps', (req, res) => {
+  const { genres = "", sort} = req.query;
+
+// Sort needs to be by rating or app
+  if(sort) {
+    if(!['Rating', 'App'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be by rating or app');
+    } 
+  }
+
+// Genre needs to be within requirements otherwise errors
+if(genres) {
+    if(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
+      return res
+        .status(400)
+        .send('Genre must be Action, Puzzle, Strategy, Casual, Arcade, Card.');
+    }
+} 
+
+let results = appData
+.filter(appName => 
+  appName
+    .Genres
+        .toLowerCase()
+        .includes(genres.toLowerCase()));
+
+// sorts rating highest to lowest
+if(sort) {
+    results
+        .sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0; }); }  
+
+if(genres) {
+    results
+        .sort((a, b) => {
+        return b-a });
+} 
+
+    res.json(results);
+});
+
+app.listen(8000, () => {
+  console.log('Server started on PORT 8000');
+});
